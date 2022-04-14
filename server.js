11846d271a5baf0b16249d2ab1fcfc06e2e9bdb8
Manuel <trapecioinv@gmail.com>
2022-04-14 01:52:34 -0300

    Heroku Deploy

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-const express = require('express');
+
 const path = require('path');
+const express = require('express');
 const app = express();
+
+// Serve static files
 app.use(express.static(__dirname + '/dist/lista-de-tareas'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/lista-de-tareas/index.html'));});
-app.listen(process.env.PORT || 8080);
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/lista-de-tareas/index.html'));
+});
+
+// default Heroku port
+app.listen(process.env.PORT || 5000);
