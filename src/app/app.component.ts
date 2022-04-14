7f0037980da019fc46814b2612aa902f06908b10
Manuel <trapecioinv@gmail.com>
2022-04-14 01:17:54 -0300

    Agregado sistema de tareas con boton 'ENTER'. Código comentado.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // Creamos la lista de tareas como un Array de strings.
   listaTareas: string[] = [];
 
+  // Agregamos la tarea si no es un string vacio.
   agregarTarea(tarea:string):void {
     if (tarea != '') {
       this.listaTareas.push(tarea);
     }
   }
-  eliminarTarea(tarea:string) {
+
+  //  Eliminamos la tarea a través del método splice().
+  eliminarTarea(tarea:string):void {
     const arr: string[] = this.listaTareas;
     for(let i: number = 0; i < arr.length; i++ ) {
       if (arr[i] == tarea) {
         arr.splice(i, 1);
       }
     } 
+  }
+
+  // Condicionamos el (ngSubmit) para evitar agregar tareas que sean strings vacios o que contengan 'null', ya que este último valor es otorgado con el método reset().
+  enviarFormulario(valorFormulario: any):void {
+    if(valorFormulario.tarea !== '' && valorFormulario.tarea !== null) {
+      this.agregarTarea(valorFormulario.tarea);
+    }
   }
 }
 
